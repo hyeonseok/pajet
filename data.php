ca20c6800c825f8c4d1b880dbae491cea1f547c6
Hyeonseok Shin <hyeonseok@gmail.com>
2012-07-08 13:46:31 +0900

    Save data when evaluation data is checked

--- data.php
+++ data.php
@@ -7,8 +7,10 @@ $page_url = $_REQUEST['url'];
 $rules = array('img', 'inputimg', 'skipnav', 'title', 'locale', 'table', 'label');
 
 foreach ($rules as $rule) {
-	foreach ($_REQUEST[$rule] as $key => $img) {
-		$str .= $page_url . ',' . $rule. ',' . $img . ',' . $_REQUEST['eval_' . $rule][$key] . "\n";
+	foreach ($_REQUEST[$rule] as $key => $path) {
+		if ($_REQUEST['eval_' . $rule][$key]) {
+			$str .= $page_url . ',' . $rule. ',' . $path . ',' . $_REQUEST['eval_' . $rule][$key] . "\n";
+		}
 	}
 }
 
