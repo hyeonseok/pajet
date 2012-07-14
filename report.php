1efd8db6134cb3efa1a4223847fb5c26ea70f8a5
Hyeonseok Shin <hyeonseok@gmail.com>
2012-07-15 00:29:43 +0900

    Report form

--- report.php
+++ report.php
@@ -1,5 +1,7 @@
 <?php
-//header('Content-Type: text/plain; charset=utf-8');
+ini_set('display_errors', 1);
+error_reporting(1);
+
 header('Content-Type: text/html; charset=utf-8');
 
 $eval_data = file('data.csv');
@@ -17,34 +19,108 @@ foreach ($eval_data as $row) {
 	$result[$url]['result'][$type][$data] = $eval;
 	$result[$url]['path'] = $path;
 }
-//print_r($result);
 
+function getFormattedText($pass, $fail) {
+	if ($pass + $fail == 0) {
+		return 'N/A';
+	} else {
+		return $pass . '/' . ($pass + $fail) . ' (' . round(($pass / ($pass + $fail)) * 1000) / 10 . '%)';
+	}
+}
+
+$result_output = array();
 foreach ($result as $url => $data_by_url) {
-	echo('<a href="' . $url . '">[' . $data_by_url['path'] . ']</a>' . "\n<br>");
+	$result_by_type = array();
 	foreach ($data_by_url['result'] as $type => $item) {
-		if ($type != 'inputimg') {
-			$c = 0;
-			$f = 0;
-			echo($type . ': ');
+		if ($type == 'inputimg' || $type == 'backgroundimage') {
+			$key = 'img';
+		} else {
+			$key = $type;
 		}
+		$pass = 0;
+		$fail = 0;
 		foreach ($item as $d => $val) {
 			if ($val == 'pass') {
-				$c++;
+				$pass++;
 			} else if ($val == 'fail') {
-				$f++;
+				$fail++;
 			}
 		}
-		if ($type != 'img') {
-			echo($c . "/" . ($c + $f) . ' (' . round($c / ($c + $f) * 1000) / 10 . '%)' . "\n<br>");
-			$total[$type]['pass'] += $c;
-			$total[$type]['fail'] += $f;
-		}
+		$result_by_type[$key]['pass'] += $pass;
+		$result_by_type[$key]['fail'] += $fail;
+		$total[$key]['pass'] += $pass;
+		$total[$key]['fail'] += $fail;
 	}
-	echo("\n<br>");
+	
+	$result_output[] = array(
+		'path' => $data_by_url['path'], 
+		'url' => $url, 
+		'alt_text' => getFormattedText($result_by_type['img']['pass'], $result_by_type['img']['fail']), 
+		'skip_navigation' => getFormattedText($result_by_type['skipnav']['pass'], $result_by_type['skipnav']['fail']), 
+		'page_title' => getFormattedText($result_by_type['pagetitle']['pass'], $result_by_type['pagetitle']['fail']), 
+		'frame_title' => getFormattedText($result_by_type['frametitle']['pass'], $result_by_type['frametitle']['fail']), 
+		'human_language' => getFormattedText($result_by_type['locale']['pass'], $result_by_type['locale']['fail']), 
+		'label' => getFormattedText($result_by_type['label']['pass'], $result_by_type['label']['fail'])
+	);
 }
 
-echo("TOTAL\n<br>");
-foreach ($total as $key => $v) {
-	echo($key . ": " . $v['pass'] . '/' . ($v['pass'] + $v['fail']) . ' (' . round($v['pass'] / ($v['pass'] + $v['fail']) * 1000) / 10 . '%)' . "\n<br>");
-}
+$total_output = array(
+	'alt_text' => getFormattedText($total['img']['pass'], $total['img']['fail']), 
+	'skip_navigation' => getFormattedText($total['skipnav']['pass'], $total['skipnav']['fail']), 
+	'page_title' => getFormattedText($total['pagetitle']['pass'], $total['pagetitle']['fail']), 
+	'frame_title' => getFormattedText($total['frametitle']['pass'], $total['frametitle']['fail']), 
+	'human_language' => getFormattedText($total['locale']['pass'], $total['locale']['fail']), 
+	'label' => getFormattedText($total['label']['pass'], $total['label']['fail'])
+);
+
+// header('Content-Type: text/plain; charset=utf-8');
+// print_r($result_output);
+// print_r($total_output);
+// exit();
 ?>
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+<meta charset="UTF-8">
+<title></title>
+</head>
+<body>
+<table>
+	<thead>
+		<tr>
+			<th>페이지</th>
+			<th>대체 텍스트</th>
+			<th>스킵 네비게이션</th>
+			<th>페이지 제목</th>
+			<th>프레임 제목</th>
+			<th>사용 언어 선언</th>
+			<th>레이블</th>
+		</tr>
+	</thead>
+	<tfoot>
+		<tr>
+			<th>합계</th>
+			<td><?php echo($total_output['alt_text']); ?></td>
+			<td><?php echo($total_output['skip_navigation']); ?></td>
+			<td><?php echo($total_output['page_title']); ?></td>
+			<td><?php echo($total_output['frame_title']); ?></td>
+			<td><?php echo($total_output['human_language']); ?></td>
+			<td><?php echo($total_output['label']); ?></td>
+		</tr>
+	</tfoot>
+	<tbody>
+<?php foreach ($result_output as $val) { ?>
+		<tr>
+			<th><a href="<?php echo($val['url']); ?>"><?php echo($val['path']); ?></a></th>
+			<td><?php echo($val['alt_text']); ?></td>
+			<td><?php echo($val['skip_navigation']); ?></td>
+			<td><?php echo($val['page_title']); ?></td>
+			<td><?php echo($val['frame_title']); ?></td>
+			<td><?php echo($val['human_language']); ?></td>
+			<td><?php echo($val['label']); ?></td>
+		</tr>
+<?php } ?>
+	</tbody>
+</table>
+</body>
+</html>
