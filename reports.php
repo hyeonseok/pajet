f8bc44b1b45edf99dcaee868b211d6adb2a38e0c
Hyeonseok Shin <hyeonseok@gmail.com>
2012-07-15 23:13:09 +0900

    Add bookmarklet test link

--- reports.php
+++ reports.php
@@ -12,5 +12,6 @@
 	<li><a href="http://cle.knou.ac.kr/">평생 교육원 사이트</a>: <a href="report.php?data=knou-cle.csv">정량평가 결과</a></li>
 	<li><a href="http://dmc.knou.ac.kr/index.jsp">디지털 미디어 센터 사이트</a>: <a href="report.php?data=knou-dmc.csv">정량평가 결과</a></li>
 </ul>
+<a href="javascript:function loadScript(scriptURL) {var scriptElem = document.createElement('SCRIPT');scriptElem.setAttribute('type', 'text/javaScript');scriptElem.setAttribute('src', scriptURL);document.body.appendChild(scriptElem);}loadScript('http://192.168.123.193:8888/tools/pajet/pajet.js');">pajet</a>
 </body>
 </html>
