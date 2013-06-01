bacc101927cf061411227483c03ba985aa6a1441
Hyeonseok Shin <hyeonseok@gmail.com>
2013-06-01 21:38:58 +0900

    Fix bookmarklet host name.

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 <body>
 <h1>PAJET</h1>
 <h2>Bookmarklet</h2>
-<p><a href="javascript:function loadScript(scriptURL) {var scriptElem = document.createElement('SCRIPT');scriptElem.setAttribute('type', 'text/javaScript');scriptElem.setAttribute('src', scriptURL);document.body.appendChild(scriptElem);}loadScript('http://<?php echo($_SERVER['REMOTE_ADDR']); ?>:8888/tools/pajet/pajet.js');">Run Pajet</a></p>
+<p><a href="javascript:function loadScript(scriptURL) {var scriptElem = document.createElement('SCRIPT');scriptElem.setAttribute('type', 'text/javaScript');scriptElem.setAttribute('src', scriptURL);document.body.appendChild(scriptElem);}loadScript('http://<?php echo($_SERVER['HTTP_HOST']); ?>/tools/pajet/pajet.js');">Run Pajet</a></p>
 <ul>
 	<li><a href="{SITEURL}">{SITENAME}</a>: <a href="report.php?data={FILENAME}">정량평가 결과</a></li>
 </ul>
